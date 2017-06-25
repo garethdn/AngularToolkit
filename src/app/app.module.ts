00758ed4b1fadedae4bde7c5d2c78856801eac78
Gareth Nolan <garethdn@gmail.com>
2017-06-25 17:09:07 +0100

    Move index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { TkSizeModule } from '../';
+import { TkSizeModule } from '../../';
 import { AppComponent } from './app.component';
 
 @NgModule({
